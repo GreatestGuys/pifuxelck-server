0dac2ddd455cf2adb6add68fd6addc3e31102fb9
jesse <number.dot.ten@gmail.com>
2014-12-06 00:05:33 -0800

    mysql password now a command line arg

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]] ; then
-  echo 'usage: ./deploy sauce-user-name'
+if [[ $# -ne 2 ]] ; then
+  echo 'usage: ./deploy sauce-user-name my-sql-password'
   exit 0
 fi
 
 cabal build pifuxelck-server-deploy
 
-ssh -f $1@everythingissauce.com 'sudo killall pifuxelck-server; sudo rm /srv/pifuxelck/pifuxelck-server;'
+ssh -f $1@everythingissauce.com "sudo killall pifuxelck-server; sudo rm /srv/pifuxelck/pifuxelck-server;"
 
 scp dist/build/pifuxelck-server-deploy/pifuxelck-server-deploy $1@everythingissauce.com:/srv/pifuxelck/pifuxelck-server
 
-ssh -f $1@everythingissauce.com 'sudo chown pifuxelck:pifuxelck /srv/pifuxelck/pifuxelck-server; sudo nohup /srv/pifuxelck/pifuxelck-server db.everythingissauce.com 3306 pifuxelck yolo2013 pifuxelck'
+ssh -f $1@everythingissauce.com "sudo chown pifuxelck:pifuxelck /srv/pifuxelck/pifuxelck-server; sudo nohup /srv/pifuxelck/pifuxelck-server db.everythingissauce.com 3306 pifuxelck $2 pifuxelck"
 
 exit 0
