a695098d8967f320d2488713d84a233b5cc8a0be
Will Coster <willcoster@gmail.com>
2014-12-06 22:42:43 -0800

    Fix --mysql-host flag in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ ssh -f $1@everythingissauce.com "sudo killall pifuxelck-server; sudo rm /srv/pif
 
 scp dist/build/pifuxelck-server/pifuxelck-server $1@everythingissauce.com:/srv/pifuxelck/pifuxelck-server
 
-ssh -f $1@everythingissauce.com "sudo chown pifuxelck:pifuxelck /srv/pifuxelck/pifuxelck-server; sudo nohup /srv/pifuxelck/pifuxelck-server --port 3000 -mysql-host db.everythingissauce.com --mysql-port 3306 --mysql-user pifuxelck --mysql-password $2 --mysql-db pifuxelck"
+ssh -f $1@everythingissauce.com "sudo chown pifuxelck:pifuxelck /srv/pifuxelck/pifuxelck-server; sudo nohup /srv/pifuxelck/pifuxelck-server --port 3000 --mysql-host db.everythingissauce.com --mysql-port 3306 --mysql-user pifuxelck --mysql-password $2 --mysql-db pifuxelck"
 
 exit 0
