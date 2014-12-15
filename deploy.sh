122401c9e9e955dac714bffa550f596d55572991
Will Coster <willcoster@gmail.com>
2014-12-14 21:42:46 -0800

    Make deploy script terminate.

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,26 @@ if [[ $# -ne 2 ]] ; then
   exit 0
 fi
 
+TMP_FILE=`mktemp /tmp/pifuxelck.XXXXXXX.sh`
+cat > $TMP_FILE << EOF
+#!/bin/bash
+echo ""
+echo -e "${BYellow}Killing existing server instances...${Color_Off}"
+killall pifuxelck-server
+
+echo ""
+echo -e "${BYellow}Launching new instance...${Color_Off}"
+cd /srv/pifuxelck/
+/usr/bin/nohup ./pifuxelck-server \
+  --port 3000 \
+  --mysql-host `dig db.everythingissauce.com +short | head -n 1` \
+  --mysql-port 3306 \
+  --mysql-user pifuxelck \
+  --mysql-password $2 \
+  --mysql-db pifuxelck 2> /dev/null > /dev/null < /dev/null &
+exit
+EOF
+
 echo ""
 echo -e "${BYellow}Building the executable...${Color_Off}"
 cabal sandbox init
@@ -16,18 +36,23 @@ cabal install --only-dependencies
 cabal build pifuxelck-server
 
 echo ""
-echo -e "${BYellow}Killing the remote server...${Color_Off}"
-#ssh $1@everythingissauce.com "sudo killall pifuxelck-server; sudo rm /srv/pifuxelck/pifuxelck-server;"
-
-echo ""
 echo -e "${BYellow}Deploying the executable...${Color_Off}"
-#scp dist/build/pifuxelck-server/pifuxelck-server $1@everythingissauce.com:/srv/pifuxelck/pifuxelck-server
+scp \
+  dist/build/pifuxelck-server/pifuxelck-server \
+  $1@everythingissauce.com:/srv/pifuxelck/pifuxelck-server
 
 echo ""
-echo -e "${BYellow}Setting permissions and running the remote server...${Color_Off}"
-ssh $1@everythingissauce.com "cd /srv/pifuxelck/; sudo chown pifuxelck:pifuxelck ./pifuxelck-server; sudo su -c 'nohup ./pifuxelck-server --port 3000 --mysql-host db.everythingissauce.com --mysql-port 3306 --mysql-user pifuxelck --mysql-password $2 --mysql-db pifuxelck &' pifuxelck;"
+echo -e "${BYellow}Deploying start up script...${Color_Off}"
+scp $TMP_FILE $1@everythingissauce.com:${TMP_FILE}
+
+echo ""
+echo -e "${BYellow}Running start up script...${Color_Off}"
+ssh \
+  $1@everythingissauce.com \
+  "chmod a+rxw $TMP_FILE && sudo su -c $TMP_FILE pifuxelck && rm $TMP_FILE"
 
 echo ""
 echo -e "${BGreen}All done!${Color_Off}"
 
+rm $TMP_FILE
 exit 0
